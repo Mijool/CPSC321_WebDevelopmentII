abe742417ef9dfb5e6d8e30cdf715b5be17da029
Camarena, Miguel <mcamarena@my.dom.edu>
2026-03-25 14:10:53 -0500

    Student app with LocalDB integration

---
+++ StudentAppMigration_C_RUD/StudentAppMigration/Models/StudentModel.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentAppMigration.Models
+{
+    public class StudentModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Major { get; set; }
+    }
+}
