caf491d15f5fb02e0f956dc1d45268ddeb40b00a
Mijool <118408982+Mijool@users.noreply.github.com>
2026-03-31 19:34:39 -0500

    A05 Done

---
+++ CPSC321_A05_MC/Models/CarModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace CPSC321_A05_MC.Models
+{
+    public class CarModel
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Make { get; set; }
+        [Required]
+        public string Model { get; set; }
+        [Required]
+        
+        [Range(1940, 2080)]
+
+        public int Year { get; set; }
+        [Required]
+        
+        [Range(0,999_999)]
+        public int Mileage { get; set; }
+        [Required]
+        public string BodyStyle { get; set; }
+        [Required]
+        public string Color { get; set; }
+
+    }
+}
